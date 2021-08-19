bee1808a79143fb5ffcafdc22a34dd746653118e
jimyungkoh <kojimyung1206@naver.com>
2021-08-19 16:01:22 +0900

    빈 생명주기 콜백 학습 내용 추가

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,15 +3,14 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = "+url);
-        /*
-        connect();
-        call("초기화 연결 메시지");
-        */
     }
 
     public void setUrl(String url){
@@ -32,14 +31,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: "+url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception{
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception{
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
