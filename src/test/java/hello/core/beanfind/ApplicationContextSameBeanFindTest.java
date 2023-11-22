13ee26f238cb340f91fe742f0bf3d45f1128966f
jimyungkoh <kojimyung@tukorea.ac.kr>
2023-11-22 17:51:37 +0900

    refactor: 출력문 대신에 slf4j 로깅 사용

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -18,48 +20,51 @@ import hello.core.member.MemoryMemberRepository;
 class ApplicationContextSameBeanFindTest {
 
     AnnotationConfigApplicationContext ac
-        = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+            = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다")
-    void findBeanByTypeDuplicate(){
+    void findBeanByTypeDuplicate() {
         //DiscountPolicy bean = ac.getBean(MemberRepository.class);
-        assertThrows(NoUniqueBeanDefinitionException.class, () -> 
-        ac.getBean (MemberRepository.class));
+        assertThrows(NoUniqueBeanDefinitionException.class, () ->
+                ac.getBean(MemberRepository.class));
     }
 
     @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈이름을 지정하면 된다")
-   void findBeanByName(){
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다")
+    void findBeanByName() {
         MemberRepository memberRepository
-        = ac.getBean("memberRepository1", MemberRepository.class);
+                = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
-   } 
+    }
 
-   @Test
-   @DisplayName("특정 타입을 모두 조회하기")
-   void findAllBeanByType(){
-       Map<String,MemberRepository> beansOfType
-       = ac.getBeansOfType(MemberRepository.class);
-       for (String key: beansOfType.keySet()){
-           System.out.println("key = "+ key + " value = "+ beansOfType.get(key));
-       }
-       System.out.println("beansOfType = " + beansOfType);
-       assertThat(beansOfType.size()).isEqualTo(2);
-   }
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType
+                = ac.getBeansOfType(MemberRepository.class);
 
-   //static class는 다른 class와 공유되지 않음.
-   @Configuration
-   static class SameBeanConfig{
+        beansOfType.forEach((k, v) -> logger.info(
+                String.format("key = %s; value = %s", k, v)));
 
-       @Bean
-       public MemberRepository memberRepository1(){
-           return new MemoryMemberRepository();
-       }
+        logger.info(String.format("beansOfType = %s", beansOfType));
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
 
-       @Bean
-       public MemberRepository memberRepository2(){
-           return new MemoryMemberRepository();
-       }
+    //static class는 다른 class와 공유되지 않음.
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
 
-   }
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+
+    }
 }
